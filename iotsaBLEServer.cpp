b05cf7f8b39224666e7a2dfab53339833a6945cf
Jack Jansen <Jack.Jansen@cwi.nl>
2019-12-04 23:43:36 +0100

    Added callback objects

--- iotsaBLEServer.cpp
+++ iotsaBLEServer.cpp
@@ -3,9 +3,28 @@
 #include "iotsaConfigFile.h"
 
 class IotsaBLEServerCallbacks : public BLEServerCallbacks {
+	void onConnect(BLEServer* pServer) {
+    IFDEBUG IotsaSerial.println("BLE connect\n");
+  }
+	void onDisconnect(BLEServer* pServer) {
+    IFDEBUG IotsaSerial.println("BLE Disconnect\n");
+
+  }
 
 };
 class IotsaBLECharacteristicCallbacks : public BLECharacteristicCallbacks {
+	void onRead(BLECharacteristic* pCharacteristic) {
+    IFDEBUG IotsaSerial.printf("BLE char onRead 0x%x\n", (uint32_t)pCharacteristic);
+  }
+	void onWrite(BLECharacteristic* pCharacteristic) {
+    IFDEBUG IotsaSerial.printf("BLE char onWrite\n");
+  }
+	void onNotify(BLECharacteristic* pCharacteristic) {
+    IFDEBUG IotsaSerial.printf("BLE char onNotify\n");
+  }
+	void onStatus(BLECharacteristic* pCharacteristic, Status s, uint32_t code) {
+    IFDEBUG IotsaSerial.printf("BLE char onStatus\n");
+  }
 
 };
 
@@ -14,7 +33,7 @@ std::string bleServiceUUID("a339b14e-9b57-41a1-8b43-14501e57f20a");
 std::string bleServiceUUID2("6e0a4baa-a5d0-40a8-a111-56ed713bb0e1");
 std::string bleCharacteristicUUID("9f25a896-a41f-4895-9ed3-645d32e64293");
 std::string bleCharacteristic2UUID("9f25a896-a41f-4895-9ed3-645d32e64293");
-std::string bleCharacteristic3UUID("9f25a896-a41f-4895-9ed3-645d32e64293");
+std::string bleCharacteristic3UUID("9932723c-c295-459d-8b97-0125e7e5f4b4");
 int bleCharacteristicProperties = BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE;
 
 BLEServer *bleServer;
@@ -57,21 +76,27 @@ void IotsaBLEServerMod::setup() {
   bleServer = BLEDevice::createServer();
   bleServer->setCallbacks(new IotsaBLEServerCallbacks());
 
-  bleService2 = bleServer->createService(bleServiceUUID2);
-  bleCharacteristic2 = bleService2->createCharacteristic(bleCharacteristic2UUID, bleCharacteristicProperties);
-  bleCharacteristic2->setValue((uint8_t *)"0042", 4);
-  bleCharacteristic2->setCallbacks(new IotsaBLECharacteristicCallbacks());
-  bleCharacteristic3 = bleService2->createCharacteristic(bleCharacteristic3UUID, bleCharacteristicProperties);
-  bleCharacteristic3->setValue((uint8_t *)"hi", 2);
-  bleCharacteristic3->setCallbacks(new IotsaBLECharacteristicCallbacks());
-  bleService2->start();
-  
   bleService = bleServer->createService(bleServiceUUID);
+  
   bleCharacteristic = bleService->createCharacteristic(bleCharacteristicUUID, bleCharacteristicProperties);
+  IFDEBUG IotsaSerial.printf("ble char1=0x%x\n", (uint32_t)bleCharacteristic);
   bleCharacteristic->setValue((uint8_t *)argument.c_str(), argument.length());
   bleCharacteristic->setCallbacks(new IotsaBLECharacteristicCallbacks());
   bleService->start();
 
+  bleService2 = bleServer->createService(bleServiceUUID2);
+
+  bleCharacteristic2 = bleService2->createCharacteristic(bleCharacteristic2UUID, bleCharacteristicProperties);
+  IFDEBUG IotsaSerial.printf("ble char2=0x%x\n", (uint32_t)bleCharacteristic2);
+  bleCharacteristic2->setValue((uint8_t *)"0042", 4);
+  bleCharacteristic2->setCallbacks(new IotsaBLECharacteristicCallbacks());
+
+  bleCharacteristic3 = bleService2->createCharacteristic(bleCharacteristic3UUID, bleCharacteristicProperties);
+  IFDEBUG IotsaSerial.printf("ble char3=0x%x\n", (uint32_t)bleCharacteristic3);
+  bleCharacteristic3->setValue((uint8_t *)"hi", 2);
+  bleCharacteristic3->setCallbacks(new IotsaBLECharacteristicCallbacks());
+  bleService2->start();
+  
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(bleServiceUUID2);  
   pAdvertising->addServiceUUID(bleServiceUUID);
